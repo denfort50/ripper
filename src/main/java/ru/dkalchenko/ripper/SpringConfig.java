a1c33befa8235c7555261f5e452c50784e0bb309
denfort50 <denfort50@yandex.ru>
2023-05-31 20:42:36 +0300

    Annotation @InjectRandomInt created

--- src/main/java/ru/dkalchenko/ripper/SpringConfig.java
+++ src/main/java/ru/dkalchenko/ripper/SpringConfig.java
@@ -2,10 +2,16 @@ package ru.dkalchenko.ripper;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.dkalchenko.ripper.quoters.InjectRandomIntAnnotationBeanPostProcessor;
 import ru.dkalchenko.ripper.quoters.TerminatorQuoter;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public InjectRandomIntAnnotationBeanPostProcessor createBeanPostProcessor(){
+        return new InjectRandomIntAnnotationBeanPostProcessor();
+    }
 
     @Bean
     public TerminatorQuoter createTerminatorQuoter() {
--- src/main/java/ru/dkalchenko/ripper/quoters/TerminatorQuoter.java
+++ src/main/java/ru/dkalchenko/ripper/quoters/TerminatorQuoter.java
@@ -5,10 +5,15 @@ import lombok.Setter;
 @Setter
 public class TerminatorQuoter implements Quoter {
 
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
 
     @Override
     public void sayQuote() {
-        System.out.println("Message = " + message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("Message = " + message);
+        }
     }
 }
